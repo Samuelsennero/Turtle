8774e1d403a350152a385a8a75ba1402a10faed0
SamuelSennero <samuel.sennero@gmail.com>
2019-11-25 10:03:15 +0100

    test 2

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,22 @@
 import turtle
+
+turtle.speed(30)
+
+turtle.color('purple', 'light blue')
+turtle.begin_fill()
+for i in range(72):
+    turtle.forward(300)
+    turtle.left(175)
+    
+turtle.end_fill()
+
+turtle.forward(130)
+turtle.color('red')
+turtle.begin_fill()
+for i in range(72):
+    turtle.forward(40)
+    turtle.left(175)
+
+turtle.end_fill()
+
+input()
